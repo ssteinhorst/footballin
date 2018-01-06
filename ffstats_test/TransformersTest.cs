eb6b3d8a665aa09abd3491e5a51aa159e5588a72
Scott Steinhorst <scott.steinhorst@gmail.com>
2018-01-05 23:54:32 -0700

    Adding reference JSON, initial test for transformers

---
+++ ffstats_test/TransformersTest.cs
@@ -0,0 +1,40 @@
+﻿using DataSync;
+using DataSync.Transformers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace ffstats_test
+{
+    class TransformersTest
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            private string eid = "2013090500";
+            
+            private Root GetTestJSONObject()
+            {
+                //string json = File.ReadAllText(@"C:\Users\Scott\Documents\Visual Studio 2015\Projects\Footballin\ffstats_test\livegamedata.json");
+                string json = File.ReadAllText(@".\livegamedata.json");
+
+                JObject parsed = JObject.Parse(json);
+                // remove unused node from JSON
+                var jsonEdited = (JObject)parsed[eid]["drives"];
+                jsonEdited.Property("crntdrv").Remove();
+                Root root = JsonConvert.DeserializeObject<Root>(parsed[eid].ToString());
+                return root;
+            }
+
+            [TestMethod]
+            public void TestAwayStatsTransformer()
+            {
+                AwayStatsTransformer ast = new AwayStatsTransformer();
+                Root root = GetTestJSONObject();
+
+                ast.TransformJSONAwayToEF(eid, "Away", root.away);
+            }
+        }
+    }
+}
